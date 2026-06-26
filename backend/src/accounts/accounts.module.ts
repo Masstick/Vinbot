94d2f15bf0b5c06ac14d56673313d48f50e7a307
Masstick <mouss.tick@wanadoo.fr>
2026-06-26 12:38:23 +0200

    feat(accounts): endpoints status + connect (start/poll)

--- backend/src/accounts/accounts.module.ts
+++ backend/src/accounts/accounts.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { VintedAccount } from './vinted-account.entity';
 import { AccountsService } from './accounts.service';
 import { VintedConnectService } from './vinted-connect.service';
+import { AccountsController } from './accounts.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([VintedAccount])],
   providers: [AccountsService, VintedConnectService],
-  controllers: [],
+  controllers: [AccountsController],
   exports: [AccountsService, VintedConnectService],
 })
 export class AccountsModule {}
